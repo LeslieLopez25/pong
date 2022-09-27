ff74a1498c37044fde0ddedf3ca50cdd199f4e04
Leslie Lopez <leslie2515lopez@gmail.com>
2022-09-27 18:01:33 -0500

    Added requestAnimationFrame

--- script.js
+++ script.js
@@ -90,9 +90,6 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Remove this
-createCanvas();
-
 // Reset Ball to Center
 function ballReset() {
   ballX = width / 2;
@@ -205,6 +202,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -220,7 +218,6 @@ function startGame() {
   createCanvas();
   animate();
   canvas.addEventListener("mousemove", (e) => {
-    console.log(e.clientX);
     playerMoved = true;
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
@@ -236,4 +233,4 @@ function startGame() {
 }
 
 // On Load
-// startGame();
+startGame();
